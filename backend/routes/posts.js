b0a958524f7f7a540c09e9fc442a1ba2c1fa566d
lhod38 <hondolidisl@gmail.com>
2019-09-27 22:17:45 +0300

    27092019A

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -67,7 +67,7 @@ router.put(
       content: req.body.content,
       imagePath: imagePath
     });
-    console.log(post);
+
     Post.updateOne({ _id: req.params.id }, post).then(result => {
       res.status(200).json({ message: "Update successful!" });
     });
